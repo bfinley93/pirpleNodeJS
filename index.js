d45a9075507674a003c642341f36cefeb3dcc667
Brandon Finley <brandonfinley1993@gmail.com>
2020-03-13 09:38:47 -0700

    merge parsingURL

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ const server = http.createServer(function(req, res){
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    // Get the HTTP Method: GET, POST, DELETE, PUT, HEAD, PATCH, OPTIONS
+    const method = req.method.toLowerCase();
+
     // Send the response
     res.end('Hello World\n');
 
     // Log the request path
-    console.log('Request received on path: '+trimmedPath);
+    console.log('Request received on path: '+trimmedPath+ ' with method: '+method);
 });
 
 // Start the server, and have it listen on port 3000
