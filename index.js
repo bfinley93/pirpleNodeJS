9c9204b5e8fe07b187b96dd9219e7fa526549283
Brandon Finley <brandonfinley1993@gmail.com>
2020-03-13 11:34:51 -0700

    added payload parsing to api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Libraries
 const http = require("http"); // hosting the server, currently locally http://localhost:3000; Wonder what this means when you put it on production.
 const url = require("url"); // making an API
+const StringDecoder = require("string_decoder").StringDecoder; // allows payloads to be read through a stream bit by bit
 
 // The server should respond to all requests with a string, meaning whenever someone visits the website or makes and api call.
 const server = http.createServer(function(req, res) {
@@ -25,11 +26,26 @@ const server = http.createServer(function(req, res) {
   // Get the headers as an object, adding keys and values to the request requires postman headers
   const headers = req.headers;
 
-  // Send the response
-  res.end("Hello World\n");
+  /* Overview of the decoder and buffer:
+   * Created a decoder that we are using later on, the buffer is a place holder for a string, as the request comes in the buffer streams
+   * in data and adds it to the buffer, the request object is going to emit a data event that we are binding on, so the req.on data then
+   * we use the decoder utf-8 to turn it into a string. Then we end the decoder, send the response, and log out the payload.
+   */
+  // Get the payload, if any
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  req.on("data", function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on("end", function() {
+    buffer += decoder.end();
 
-  // Log the request path
-  console.log("request received with these headers", headers);
+    // Send the response
+    res.end("Hello World\n");
+
+    // Log the request path
+    console.log("Request received with this payload: ", buffer);
+  });
 });
 
 // Start the server, and have it listen on port 3000
